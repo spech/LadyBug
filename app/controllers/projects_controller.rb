8ac2b1f6f40995f73b9efd4d559fd7154c316402
Sebastien Pech <sebastien.pech@gmail.com>
2013-08-05 10:35:07 +0200

    small views improvement

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -33,7 +33,7 @@ class ProjectsController < ApplicationController
 	def update
 		respond_to do |format|
 		    if @project.update(project_params)
-		    	format.html { redirect_to @project, notice: 'Issue was successfully updated.' }
+		    	format.html { redirect_to @project, notice: 'project was successfully updated.' }
 		        format.json { head :no_content }
 		    else
 		        format.html { render action: 'edit' }
