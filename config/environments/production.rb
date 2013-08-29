2fc29b80cfdedbd4df2ee837891bff2f1ddd8399
Sebastien Pech <sebastien.pech@gmail.com>
2013-08-29 16:45:36 +0200

    fixing new user form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ LadyBug::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => 'fierce-mesa-5050.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
